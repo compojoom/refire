4391d4d129198dc9cd54b49f991716b553d11193
Lari Hoppula <lari@hoppula.com>
2016-03-28 23:47:35 +0300

    Fixed code style errors in actions

--- src/actions/firebase.js
+++ src/actions/firebase.js
@@ -23,9 +23,12 @@ export const WRITE_ERRORS_UPDATED = "WRITE_ERRORS_UPDATED"
 // generated UUIDs are only used for internal request tracking
 // http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
 function createUUID() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-    return v.toString(16);
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(char) {
+    const rand = Math.random()*16|0
+    const value = char === 'x'
+      ? rand
+      : (rand&0x3|0x8)
+    return value.toString(16)
   })
 }
 
